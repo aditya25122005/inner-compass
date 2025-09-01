96b7e423ceec9bb16622ef267d5ea65ed85148f6
aditya25122005 <aditya25pda@gmail.com>
2025-09-01 15:15:52 +0530

    Add client folder as a normal directory

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Inner Compass</h1>
+      <p>This is the start of your mental wellness platform!</p>
+    </div>
+  );
+}
+
+export default App;
